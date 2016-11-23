e79c96a53708b2e1ddcee66c570586233a00f70e
Matthias Monnier <matthias.monnier@keyinfuser.com>
2016-11-23 17:48:50 +0100

    Before make oldconfig, last image

--- board/keyinfuser/post_build.sh
+++ board/keyinfuser/post_build.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 echo -e "\nPermitRootLogin yes" >>  "${TARGET_DIR}/etc/ssh/sshd_config"
 echo -e "\nauto eth0\niface eth0 inet dhcp" >> "${TARGET_DIR}/etc/network/interfaces"
