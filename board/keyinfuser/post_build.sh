efb756a4f97e09978491c5881a4228b13fd512fa
Matthias Monnier <matthias.monnier@keyinfuser.com>
2016-11-25 10:16:14 +0100

    Add some fixes in post_build.sh

--- board/keyinfuser/post_build.sh
+++ board/keyinfuser/post_build.sh
@@ -1,4 +1,23 @@
 #!/bin/bash
 
-echo -e "\nPermitRootLogin yes" >>  "${TARGET_DIR}/etc/ssh/sshd_config"
-echo -e "\nauto eth0\niface eth0 inet dhcp" >> "${TARGET_DIR}/etc/network/interfaces"
+append_if_not_present() {
+    grep -x "$1" "$2" > /dev/null
+    if [ "$?" -ne "0" ]; then
+	echo "Patching $2";
+        echo -e "\n$1" >> "$2";
+    fi
+}
+
+append_if_not_present "PermitRootLogin yes"	"${TARGET_DIR}/etc/ssh/sshd_config"
+append_if_not_present "auto eth0"		"${TARGET_DIR}/etc/network/interfaces"
+append_if_not_present "iface eth0 inet dhcp"	"${TARGET_DIR}/etc/network/interfaces"
+
+# Break the symlink between /var/tmp and /tmp for the timedatectl command
+rm -f ${TARGET_DIR}/var/tmp
+mkdir ${TARGET_DIR}/var/tmp
+chmod 1777 ${TARGET_DIR}/var/tmp
+
+
+# Add networkd service for starting eth0 at boot
+
+append_if_not_present "[Match]\nname=eth0\n\n[Network]\nDHCP=yes\n"     "${TARGET_DIR}/etc/systemd/network/eth0.network"
